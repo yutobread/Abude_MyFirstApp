1e42cc0b01896eb5f40bc6580f50affde2ffbbbd
thei <aAbude@mcm.edu.ph>
2022-06-01 07:54:34 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/abude_myfirstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.abude_myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
